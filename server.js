d401861017aceb680aadee77fee900af7e0e393e
Sandro Mendoza <luxe29688@gmail.com>
2022-11-23 09:23:49 -0500

    updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express ");
-const path = require("path ");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static("./dist/pokemon"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/pokemon/ " });
+  res.sendFile("index.html", { root: "dist/pokemon/" });
 });
 app.listen(process.env.PORT || 8080);
